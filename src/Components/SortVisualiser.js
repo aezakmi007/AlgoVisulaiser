32578be39fafe95f5bcde35b7426568919b19175
aezakmi007 <abdullahfarhan207@gmail.com>
2021-08-04 09:06:47 +0530

    Added generate new array button and displayed the sorting bars

--- src/Components/SortVisualiser.js
+++ src/Components/SortVisualiser.js
@@ -1,41 +1,38 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from "react";
+import "./sortingVisualiser.css";
 
 const rnadomIntFromInterval = (min, max) => {
-
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
-
-
-const  SortVisualiser = () => {
-
+const SortVisualiser = () => {
   const [array, setArray] = useState([]);
 
   const resetArray = () => {
     const arr = [];
-    for(let i = 0; i<100; i++){
-      arr.push(rnadomIntFromInterval(5, 1000));
+    for (let i = 0; i < 310; i++) {
+      arr.push(rnadomIntFromInterval(5, 600));
     }
     setArray(arr);
-  
-  }
+  };
   useEffect(() => {
     resetArray();
   }, []);
 
-
   return (
-   <>
-   {array.map((value, idx) => {
-     return (
-     <div  key = {idx}>
-     {value}
-     </div>
-   )
-   })}
-   </>
-  )
-}
+    <div className="array-container">
+      {array.map((value, idx) => {
+        return (
+          <div
+            className="array-bar"
+            style={{ backgroundColor: "black", height: `${value}px` }}
+            key={idx}
+          ></div>
+        );
+      })}
+      <button onClick={() => resetArray()}>Generate New Array</button>
+    </div>
+  );
+};
 
 export default SortVisualiser;
